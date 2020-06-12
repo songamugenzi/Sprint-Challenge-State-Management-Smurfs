7d33bef4d495cb1fb649723c06903f54f6e30db8
S Mugenzi <smugenzi@Ss-MacBook-Air.local>
2020-06-12 14:02:10 -0400

    Met project MVP

---
+++ smurfs/src/actions/index.js
@@ -0,0 +1,34 @@
+import axios from 'axios';
+
+export const FETCH_SMURFS = 'FETCH_SMURFS';
+export const DISPLAY_SMURFS = 'DISPLAY_SMURFS';
+export const CREATE_SMURF = 'CREATE_SMURF';
+export const SET_ERROR = 'SET_ERROR';
+
+export const visitVillage = () => dispatch => {
+    dispatch({ type: FETCH_SMURFS });
+    axios
+        .get('http://localhost:3333/smurfs')
+        .then(res => {
+            console.log('smurf get request', res);
+            dispatch({ type: DISPLAY_SMURFS, payload: res.data })
+        })
+        .catch(err => {
+            console.log('Oh no! 😪 The Smurf village is closed for the day', err);
+            dispatch({ type: SET_ERROR, payload: err.data })
+        });
+}
+
+export const buildVillage = (newbornSmurf) => dispatch => {
+    dispatch({ type: CREATE_SMURF });
+    axios
+        .post('http://localhost:3333/smurfs', newbornSmurf)
+        .then(res => {
+            console.log('New village Smurf created!', res);
+            dispatch({ type: DISPLAY_SMURFS, payload: res.data })
+        })
+        .catch(err => {
+            console.log('Oh no! 😪 the Smurf factory is closed', err);
+            dispatch({ type: SET_ERROR, payload: err.data })
+        })
+}
--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,16 +1,38 @@
-import React, { Component } from "react";
-import "./App.css";
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>SMURFS! 2.0 W/ Redux</h1>
-        <div>Welcome to your state management version of Smurfs!</div>
-        <div>Start inside of your `src/index.js` file!</div>
-        <div>Have fun!</div>
-      </div>
-    );
+import React from "react";
+import { connect } from 'react-redux';
+import Form from './CreateSmurfForm';
+
+const App = props => {
+
+  return (
+    <div className="App">
+      <Form />
+      <ul>
+
+        {
+          props.villagers.map((smurf, idx) => {
+            return (
+              <li key={idx}>
+                {smurf.name}
+              </li>
+            )
+          })
+        }
+      </ul>
+
+    </div>
+  );
+}
+
+const mapStateToProps = state => {
+  return {
+    villagers: state.villagers
   }
 }
 
-export default App;
+export default connect(
+  mapStateToProps,
+  {}
+)(App);
+
+
